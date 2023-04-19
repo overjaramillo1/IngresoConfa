84bf92fcaa302a9a48381ad7b539579f318bfc3f
overjaramillo1 <jaramil161@gmail.com>
2023-04-19 13:52:59 -0500

    changes for vingreso1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,21 @@
 import React, { useState, useContext,useEffect } from 'react';
-import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import { CognitoUser, AuthenticationDetails,CognitoUserPool } from "amazon-cognito-identity-js";
 import UserPool from "./UserPool";
 import "./form.css";
 import {useHistory } from "react-router-dom";
 //https://docs.aws.amazon.com/es_es/apigateway/latest/developerguide/apigateway-enable-cognito-user-pool.html
 //https://www.youtube.com/watch?v=yhD2XJVFQUg&list=PLDckhLrNepPR8y-9mDXsLutiwsLhreOk1&index=2
-
-//import { useAuthContext } from '../contexts/auth-context';
+import { gapi } from "gapi-script";
 import jwtDecode from 'jwt-decode';
 import { GoogleLogin } from 'react-google-login';
 
 
 const Login = () => {
 
-
+  const poolData = {
+    UserPoolId: 'us-east-1_MQwendKCy',
+    ClientId: '7fh5usiukn07fht43mv3hp35o0'
+  };
   
  // const authContext = useAuthContext();
   // const navigate = useNavigate();
@@ -23,12 +25,12 @@ const Login = () => {
   const [respuestaLogin, setRespuestaLogin] = useState("");
  // const [authenticated, setAuthenticated] = useState(false);
 
-
+/*
  useEffect(() => {
   window.gapi.load("auth2", () => {
     window.gapi.auth2
       .init({
-        client_id: "261980996205-hjhh2e8edvs51td1ks19jdgvhui60dh7.apps.googleusercontent.com",
+        client_id: "261980996205-m4h16c4eus7auq03haj0v3rrjksqi38c.apps.googleusercontent.com",
       })
       .then((auth) => {
         const currentUser = auth.currentUser.get();
@@ -37,18 +39,47 @@ const Login = () => {
         }
       });
   });
-}, []);
+}, []);*/
 
+const options = {
+  scope: 'email profile',
+  prompt: 'select_account'
+};
 
- const handleGoogleLogin = () => {
-  window.gapi.auth2.getAuthInstance().signIn().then((googleUser) => {
-    console.log("Usuario autenticado:", googleUser.getBasicProfile());
-  });
+/*const googleAuth = new GoogleAuth({
+  clientId: '261980996205-hjhh2e8edvs51td1ks19jdgvhui60dh7.apps.googleusercontent.com'
+});
+const handleGoogleLogin = async () => {
+  try {
+    const response = await googleAuth.signIn();
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};*/
+/*
+const handleGoogleLogin = async (response) => {
+  const { tokenId } = response;
+  
+  const authenticationData = {
+    Token: tokenId
+  };*/
+ const handleGoogleLogin = (response) => {
+ // window.gapi.auth2.getAuthInstance().signIn().then((googleUser) => {
+  //  console.log("Usuario autenticado:", googleUser.getBasicProfile());
+ // });
+
+ const { tokenId } = response;
+  
+  const authenticationData = {
+    Token: tokenId
+  };
+  console.log(response);
 };
 
  const responseGoogle = (response) => {
   console.log("response>"+JSON.stringify(response));
-  history.push("/IngresoConfa");
+ // history.push("/IngresoConfa");
   console.log('responseGoogle :>> ', response);
   var profile = response.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -108,6 +139,33 @@ const responseGoogleError = (error) => {
       },
     });
   };
+  const userPool = new CognitoUserPool(poolData);
+
+  async function handleGoogleSignIn() {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+    const googleUser = await auth2.signIn();
+    const accessToken = googleUser.getAuthResponse().id_token;
+  
+    const cognitoUser = new CognitoUser({
+      Username: googleUser.getBasicProfile().getEmail(),
+      Pool: userPool
+    });
+    const authDetails = new AuthenticationDetails({
+      Username: googleUser.getBasicProfile().getEmail(),
+      Password: accessToken
+    });
+    cognitoUser.authenticateUser(authDetails, {
+      onSuccess: function (result) {
+        console.log('Usuario autenticado correctamente en AWS Cognito', result);
+      },
+      onFailure: function (err) {
+        console.error(err);
+      },
+      newPasswordRequired: function () {},
+      mfaRequired: function () {},
+      customChallenge: function () {}
+    });
+  }
 
   return (
     <div>
@@ -135,19 +193,16 @@ const responseGoogleError = (error) => {
         </label>
         <input type="submit" name="Login" value="Login" />
         <label><h1>{respuestaLogin}</h1></label>
-        <GoogleLogin //261980996205-m4h16c4eus7auq03haj0v3rrjksqi38c.apps.googleusercontent.com
-        clientId="261980996205-hjhh2e8edvs51td1ks19jdgvhui60dh7.apps.googleusercontent.com"
-        buttonText="Iniciar sesión con Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogleError}
-        isSignedIn={false}
-        hostedDomain={null}
-        uxMode='popup'
-        SameSite='None'
-       // cookiePolicy={'single_host_origin'}
-      />
-      <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
+
+
       </form>
+      <GoogleLogin
+        clientId="261980996205-nm2bsterdg3vqdp00qqaan8mho4tvl5t.apps.googleusercontent.com"
+        onSuccess={handleGoogleLogin}
+        onFailure={handleGoogleLogin}
+      />
+            <button onClick={handleGoogleSignIn}>Ingresar con Google ok</button>
+
 
     </div>
   );
